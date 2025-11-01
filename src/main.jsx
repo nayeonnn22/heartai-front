f75629041584704c283e570a496d13d0531a368d
peachily <peachily1108@gmail.com>
2025-11-01 23:54:00 +0900

    init: react frontend setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
